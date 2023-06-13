68243dd34f9ca68ba0304b42c3f76eaa09706451
alamin <amin.se.aiub@gmail.com>
2023-06-13 17:15:15 +0600

    intro php

---
+++ php/test.php
@@ -0,0 +1,48 @@
+<?php 
+
+    // $name = "alamin";
+    // $id = 12;
+    // $cgpa = 2.5;
+    
+     $student = ['alamin', 12, 3.5];
+    // $students = [
+    //                 ['alamin', 12, 3.5],
+    //                 ['alamin', 12, 3.5],
+    //                 ['alamin', 12, 3.5]
+    //             ];
+    // $students[2][2];
+
+    // $student = ['name'=>'alamin', 'id'=>12, "cgpa"=>3.5];
+    // $students = [
+    //                 's1'=>['name'=>'alamin', 'id'=>12, "cgpa"=>3.5],
+    //                 's2'=>['name'=>'alamin', 'id'=>12, "cgpa"=>3.5],
+    //                 's3'=>['name'=>'alamin', 'id'=>12, "cgpa"=>3.5]
+    //             ];
+    // $students['s1']['name'];
+
+    // for($i=0; $i<10; $i++){
+
+    // }
+
+    // if(){
+
+    // }else{
+
+    // }
+
+    // function abc($id=0, $name='alamin'){
+    //     return 12;
+    // }
+
+    // $id = abc();
+
+    // print("Hello PHP \r\n");
+    // echo "Test";
+
+    // for($i=0; $i<10; $i++){
+    //     echo "Test \r\n";
+    // }
+
+    print_r($student);
+
+?>
