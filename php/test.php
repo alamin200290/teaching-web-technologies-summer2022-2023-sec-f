3186fda181e3301f524d0eb2afb2819d80530c3d
alamin <amin.se.aiub@gmail.com>
2023-06-18 17:21:13 +0600

    php form submit and basic validation

--- php/test.php
+++ php/test.php
@@ -1,48 +1,17 @@
 <?php 
 
-    // $name = "alamin";
-    // $id = 12;
-    // $cgpa = 2.5;
-    
-     $student = ['alamin', 12, 3.5];
-    // $students = [
-    //                 ['alamin', 12, 3.5],
-    //                 ['alamin', 12, 3.5],
-    //                 ['alamin', 12, 3.5]
-    //             ];
-    // $students[2][2];
+    //print_r($_GET);
+    $username = $_GET['username'];
+    $password = $_GET['password'];
 
-    // $student = ['name'=>'alamin', 'id'=>12, "cgpa"=>3.5];
-    // $students = [
-    //                 's1'=>['name'=>'alamin', 'id'=>12, "cgpa"=>3.5],
-    //                 's2'=>['name'=>'alamin', 'id'=>12, "cgpa"=>3.5],
-    //                 's3'=>['name'=>'alamin', 'id'=>12, "cgpa"=>3.5]
-    //             ];
-    // $students['s1']['name'];
-
-    // for($i=0; $i<10; $i++){
-
-    // }
-
-    // if(){
-
-    // }else{
-
-    // }
-
-    // function abc($id=0, $name='alamin'){
-    //     return 12;
-    // }
-
-    // $id = abc();
-
-    // print("Hello PHP \r\n");
-    // echo "Test";
-
-    // for($i=0; $i<10; $i++){
-    //     echo "Test \r\n";
-    // }
-
-    print_r($student);
-
+    if($username == "" || $password == ""){
+        //echo "null username/password";
+        header('location: index.php?msg=null');
+    }else if($username == $password){
+        //echo "valid user!";
+        header('location: home.html');
+    }else{
+        //echo "invalid username/password";
+        header('location: index.php?msg=invalid');
+    }
 ?>
