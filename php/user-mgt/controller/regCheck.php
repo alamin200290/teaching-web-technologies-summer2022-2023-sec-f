bc5bfcdf833756ffa70be5bc2723d4da0c31aa61
alamin <amin.se.aiub@gmail.com>
2023-07-05 16:44:41 +0600

    file upload

--- php/user-mgt/controller/regCheck.php
+++ php/user-mgt/controller/regCheck.php
@@ -10,6 +10,18 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         $email = $_POST['email'];
+        
+       // print_r($_FILES);
+
+        $src = $_FILES['myfile']['tmp_name'];
+        $des = "../upload/".$_FILES['myfile']['name'];
+
+
+        if(move_uploaded_file($src, $des)){
+            echo "success";
+        }else{
+            echo "error";
+        }
 
         $status = addUser($username, $password, $email);
 
--- php/user-mgt/view/signup.php
+++ php/user-mgt/view/signup.php
@@ -3,10 +3,11 @@
     <title>Signup Page</title>
 </head>
 <body>
-        <form method="POST" action="../controller/regCheck.php" enctype="">
+        <form method="POST" action="../controller/regCheck.php" enctype="multipart/form-data">
             username:   <input type="text" name="username" value="" /><br>
             password:   <input type="password" name="password" value="" /> <br>
             Email:      <input type="email" name="email" value="" /> <br>
+            Image:      <input type="file" name="myfile" value="" /> <br>
                         <input type="submit" name="submit" value="Submit" />
                         <a href="login.php"> Login </a> 
         </form>    
